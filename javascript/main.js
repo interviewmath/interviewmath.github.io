2e683570d4dac3cd58f34e11356467263b9e2c05
jackhyder <jackhyder20@gmail.com>
2017-06-07 18:50:20 -0700

    creates initial HTML, CSS, and JS for application

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+var Timeout;
+
+(function(){
+    InterviewMath.Data.Questions = [InterviewMath.Data.Questions.shift()].concat(InterviewMath.Data.Questions.sort(function(){return 0.5 - Math.random()}));
+    InterviewMath.Data.Tips.sort(function(){return 0.5 - Math.random()});
+
+
+})();
---
+++ javascript/questions.js
@@ -0,0 +1,13 @@
+var InterviewMath = {};
+
+InterviewMath.Data = {};
+
+InterviewMath.Data.Questions = [
+    {"question":"Tell me about yourself.", "tag":"GENERAL", "src":"ctpmi"},
+    {"question":"Why should we hire you?", "tag":"GENERAL", "src":"web"},
+    {"question":"Why do you want to work here?", "tag":"GENERAL", "src":"web"}
+];
+
+InterviewMath.Data.Passed = [];
+
+InterviewMath.Data.Tips = [];
