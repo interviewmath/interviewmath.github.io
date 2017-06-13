83ce4ff72da328985491d52f854f048af289b088
jackhyder <jackhyder20@gmail.com>
2017-06-12 19:54:38 -0700

    Added JavaScript functions and some Bootstrap-specific CSS

---
+++ javascript/functions.js
@@ -0,0 +1,105 @@
+var subCategory = "ALL CATEGORIES"
+
+function next_question(){
+
+    if ($('.sidebar').hasClass('hide')){
+        $('.sidebar').removeClass('hide');
+        showFilters();
+        Timeout = setTimeout(hideFilters, 5000)
+    }
+
+    $('.desktop_controls').hide();
+    $('.mobile_controls').hide();
+
+    var newQuestion = InterviewMath.Data.Questions.shift();
+
+    if (newQuestion){
+        $('#question').removeClass('hide');
+        $('#timer').show();
+        $('#answer').removeClass('hide');
+        $('#end').addClass('hide');
+
+        var question = newQuestion['question'];
+        var sTag = newQuestion['tag'];
+        var sSource = newQuestion['src'];
+
+        if (subCategory == "ALL CATEGORIES" || sTag == subCategory){
+            $('#question_text').text(question);
+            timer_restart();
+        }
+        else {
+            InterviewMath.Data.Passed.push(newQuestion);
+            next_question();
+        }
+
+        $('#answer').addClass('hide');
+
+        InterviewMath.Data.Questions = InterviewMath.Data.Passed;
+        InterviewMath.Data.Passed = [];
+
+        timer_stop();
+        $('#question').addClass('hide');
+        $('#timer').hide();
+        $('#end').removeClass('hide');
+
+        if (InterviewMath.Data.Questions.length == 0){
+            $('#end').children('h1').text("All questions answered.")
+        }
+    }
+}
+
+function showFilters(){
+    $('.filter').each(function(index){
+        animateIn($(this), index*100)
+    })
+}
+
+function hideFilters(){
+    var filter = $('.filter').length
+    $('.filter').each(function(index){
+        animateOut($(this), (filter - index - 1)*100)
+    })
+}
+
+function animateIn($filter, delay){
+    setTimeout(function(){
+        $filter.css({"margin-right": "0px"})
+    }, delay)
+}
+
+function animateOut($filter, delay){
+    setTimeout(function(){
+        $filter.css({"margin-right": "-400px"})
+    }, delay)
+}
+
+// Timer Functions
+
+var timer;
+
+function timer_tick(){
+
+    timer = timer + 1;
+
+    var seconds = timer%60;
+
+    if (seconds <10){
+        seconds = '0' + seconds;
+    }
+
+    var minutes = (timer - seconds)/60;
+    $('#timer').html(minutes + ':' + seconds);
+
+    setTimeout('timer_tick();', 1000);
+}
+
+function timer_restart(){
+    timer = 0;
+
+    $('#timer').html('0:00');
+    $('#timer').show();
+}
+
+function timer_stop(){
+    $('.link').addClass('end');
+}
--- javascript/main.js
+++ javascript/main.js
@@ -4,5 +4,58 @@ var Timeout;
     InterviewMath.Data.Questions = [InterviewMath.Data.Questions.shift()].concat(InterviewMath.Data.Questions.sort(function(){return 0.5 - Math.random()}));
     InterviewMath.Data.Tips.sort(function(){return 0.5 - Math.random()});
 
+    $(document).ready(function(){
 
+        timer_tick();
+        $('#timer').hide();
+
+        $('#dropdown_selector').click(function(event){
+            $(this).toggleClass('active');
+        })
+
+        var $cat = $('filter:last-child');
+
+        $('.filter').addClass('text-grey');
+        $(this).removeClass('text-grey');
+
+        subCategory = $(this).text().toUpperCase();
+        if($('.desktop_controls').css('display') == "none" && $('.mobile_controls').css('display') == "none"  && $(this).text() !== 'ALL CATEGORIES'){
+            next_question();
+        }
+
+        $cat.detach();
+
+        $(document).click(function(e){
+            if (e.target !== $('#current_selector')[0]){
+                if ($('#dropdown_selector').hasClass('active')){
+                    $('#dropdown_selector').removeClass('active');
+                    return 0;
+                }
+            }
+        })
+
+        $("#slider").click(function(e){
+            if (e.target === $(this)[0]){
+                if ($('#dropdown_selector').hasClass('active')){
+                    $('#dropdown_selector').removeClass('active');
+                    return 0;
+                }
+                else {
+                    next_question();
+                }
+            }
+        });
+
+        $(document).keypress(function(e){
+            switch (e.which){
+                case 13:
+                    next_question();
+                    break;
+            }
+        });
+
+        $('.sidebar_right').mouseleave(function(){
+            Timeout = setTimeout(hideFilters, 5000)
+        })
+    })
 })();
